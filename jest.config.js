9343195a985f737343d67a94e2816db513cc2b15
Rudger Gravestein <rgravestein+pv@lionsville.nl>
2025-02-26 13:51:47 +0100

    Update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
 module.exports = {
-    testMatch: ['**/src/**/tests/**/*.test.ts'],
-    testPathIgnorePatterns: ["/node_modules/"],
+    testMatch: [
+        '<rootDir>/src/**/tests/**/*.test.ts',
+    ],
+    testPathIgnorePatterns: [
+        "/node_modules/",
+    ],
     reporters: ["default"],
     globalSetup: "./testSetup.js",
     testEnvironment: "node"
